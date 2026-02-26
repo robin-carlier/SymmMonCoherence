4b80b0d6ec86d61b054331adf26d39cbec2d9cde
Robin Carlier <57142648+robin-carlier@users.noreply.github.com>
2026-02-26 07:59:27 +0100

    chore: add missing copyrights

--- SymmMonCoherence/ForMathlib/CategoryTheory/Bicategory/Adjunction/Mates.lean
+++ SymmMonCoherence/ForMathlib/CategoryTheory/Bicategory/Adjunction/Mates.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Robin Carlier. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Robin Carlier
+-/
 module
 
 public import Mathlib.CategoryTheory.Bicategory.Adjunction.Mate
